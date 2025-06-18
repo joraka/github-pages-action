b05bc40bc548f0b43d142cc03160553f6194647a
Jonas <92853498+joraka@users.noreply.github.com>
2025-06-18 21:52:07 +0300

    10k files test

--- build.js
+++ build.js
@@ -1,6 +1,27 @@
 const fs = require("fs");
 let indexData = fs.readFileSync("./src/index.html", "utf-8");
 
-indexData = indexData.replace('{{time}}', new Date().toISOString());
+indexData = indexData.replace("{{time}}", new Date().toISOString());
 
-fs.writeFileSync('./public/index.html', indexData);
+const files = [];
+
+const filesPath = "./public/files";
+try {
+  fs.mkdirSync(filesPath);
+} catch (err) {}
+
+for (let i = 0; i < 10000; i++) {
+//   if (i > 10) break;
+  const fileName = `${i}.txt`;
+  fs.writeFileSync(`${filesPath}/${fileName}`, `${i} | ${parseInt(Math.random() * 1e16, 10)}`);
+  files.push(fileName);
+}
+
+indexData = indexData.replace(
+  "{{links}}",
+  files.map((fileName) => `<a href="files/${fileName}">${fileName}</a>`).join("")
+);
+
+fs.writeFileSync("./public/index.html", indexData);
+
+console.log("files built");
